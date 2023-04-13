265c4a6bc87625fd41603f4b80e3caa94afe29f1
Theja Swaroop <105914391+Swarooptheja@users.noreply.github.com>
2023-04-13 17:47:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
-function fizzbuzz(num) {
-  const sum = [...num].reduce((acc, digit) => acc + parseInt(digit), 0);
-  for (let i = 1; i <= sum; i++)
-  {
-    if (i % 4 === 0 && i % 5 === 0) 
-    {
-      console.log("FizzBuzz");
-    } 
-    else if (i % 4 === 0) 
-    {
-      console.log("Fizz");
-    } 
-    else if (i % 5 === 0)
-    {
-      console.log("Buzz");
-    }
-    else
-    {
-      console.log(i);
+function fizzbuzz(number) {
+  const number_sum = [...number].reduce((acc, digit) => acc + parseInt(digit), 0); // calculate the sum of the digits in the phone number
+  for (let i = 1; i <= number_sum; i++) {
+    if (i % 4 === 0 && i % 5 === 0) {
+      process.stdout.write("FizzBuzz ");
+    } else if (i % 4 === 0) {
+      process.stdout.write("Fizz ");
+    } else if (i % 5 === 0) {
+      process.stdout.write("Buzz ");
+    } else {
+      process.stdout.write(i+" " );
     }
   }
 }
+
 fizzbuzz("9000000099")
